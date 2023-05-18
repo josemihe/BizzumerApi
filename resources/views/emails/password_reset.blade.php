af8a9064e90670a7a3699480693b4297a3d524e7
josemihe <josemihe@iescastelar.com>
2023-05-18 11:51:48 +0100

    Api finished

--- resources/views/emails/password_reset.blade.php
+++ resources/views/emails/password_reset.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Password Reset Request</title>
-</head>
-<body>
-<p>Hello {{ $user->email }},</p>
+    <head>
+        <meta charset="UTF-8">
+        <title>Password Reset Request</title>
+    </head>
+    <body>
+        <p>Hello {{ $user->email }},</p>
 
-<p>You have requested a password reset for your account. Please use the following token to reset your password in the app:</p>
+        <p>You have requested a password reset for your account. Please use the following token to reset your password in the app:</p>
 
-<p>{{ $user->reset_token }}</p>
+        <p>{{ $user->reset_token }}</p>
 
-<p>If you did not request a new password, please ignore this email.</p>
+        <p>If you did not request a new password, please ignore this email.</p>
 
-<p>Thank you,</p>
+        <p>Thank you,</p>
 
-<p>The Bizzumer Team</p>
-</body>
+        <p>The Bizzumer Team</p>
+    </body>
 </html>
